262b55112daeaccfc0b60d2f19fb41fe1603ab07
Shani <plant.shani@gmail.com>
2025-02-06 21:43:46 +0200

    feat: add condition mapping dictionary for stress types

---
+++ src/utils/constants.py
@@ -0,0 +1,18 @@
+# Condition Mapping Dictionary
+CONDITION_DICT = {
+    'W': 'Control',
+    'C': 'Cutting',
+    'DT': 'Drought - Terminal',
+    'DP': 'Drought - With some irrigation',
+    'S': 'Salt',
+    'B': 'Biotic stress',
+    'NR': 'Not relevant'
+}
+
+CONDITION_DICT_TEXT_F = {'W': 'Control',
+ 'C': 'Cutting',
+ 'DT': 'Drought\nTerminal',
+ 'DP': 'Drought\nWith some irrigation',
+ 'S': 'Salt',
+ 'B': 'Biotic stress',
+ 'NR': 'Not relevant'}
